690be179b4994ec4c0ede31f62fa32b18c24f069
Javier de Arcos <javier.dearcos@liferay.com>
2021-12-23 21:50:40 +0100

    Add endpoint to create user

--- server.js
+++ server.js
@@ -1,17 +1,42 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+const userRepository = require('./userRepository')
 
-app.use(cors())
-app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post("/api/users", (req, res) => {
+  
+  const username = req.body.username;
 
+  if (!username) {
+    res.json({error: '"username" is required in body request'});
+    return;
+  }
 
+  userRepository.findByUsername(username, (_, existingUser) => {
+    if (existingUser) {
+      res.json(existingUser);
+      return;
+    }
 
+    userRepository.createUser(req.body.username, (err, user) => {
+      if (err) {
+        res.json({ error: `Error creating user ${username}: ${err}`});
+        return;
+      }
+  
+      res.json(user);
+    });
+  });
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
---
+++ userRepository.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new Schema({
+  username:  { type: String, required: true },
+});
+
+const User = mongoose.model("User", userSchema);
+
+const createUser = (username, done) => {
+  const user = new User({
+    username
+  });
+
+  user.save(done);
+};
+
+const findById = (id, done) => {
+  User.findById(id, done);
+};
+
+const findByUsername = (username, done) => {
+  User.findOne({username}, done);
+}
+
+module.exports = {
+  createUser,
+  findById,
+  findByUsername
+}
