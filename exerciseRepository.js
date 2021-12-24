ab01e5dfb38dd9697e6849174737d92510512503
Javier de Arcos <javier.dearcos@liferay.com>
2021-12-24 07:50:35 +0100

    Implement endpoint to create an exercise

---
+++ exerciseRepository.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const exerciseSchema = new Schema({
+  userId:  { type: String, required: true },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date, default: new Date() }
+});
+
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+const createExercise = (userId, description, duration, date, done) => {
+  const exercise = new Exercise({
+    userId,
+    description,
+    duration,
+    date
+  });
+
+  exercise.save(done);
+};
+
+module.exports = {
+  createExercise
+}
--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
-const userRepository = require('./userRepository')
+const res = require('express/lib/response');
+const exerciseRepository = require('./exerciseRepository');
+const userRepository = require('./userRepository');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
@@ -14,29 +16,62 @@ app.get('/', (req, res) => {
 
 app.post("/api/users", (req, res) => {
   
-  const username = req.body.username;
+  const username = getRequiredBodyProperty(req, "username");
 
-  if (!username) {
-    res.json({error: '"username" is required in body request'});
-    return;
-  }
-
-  userRepository.findByUsername(username, (_, existingUser) => {
-    if (existingUser) {
-      res.json(existingUser);
+  userRepository.createUser(req.body.username, (err, user) => {
+    if (err) {
+      res.json({ error: `Error creating user "${username}": ${err}`});
       return;
     }
 
-    userRepository.createUser(req.body.username, (err, user) => {
+    res.json(user);
+  });
+});
+
+app.post("/api/users/:userId/exercises", (req, res) => {
+  const userId = req.params.userId;
+  const description = getRequiredBodyProperty(req, "description");
+  const duration = getRequiredBodyProperty(req, "duration");
+  const date = req.body.date;
+
+  userRepository.findById(userId, (err, user) => {
+    if (err) {
+      res.json({ error: `Error getting user with id "${userId}": ${err}`});
+      return;
+    }
+
+    if (!user) {
+      res.json({ error: `User with id "${userId}" does not exist`});
+      return;
+    }
+
+    exerciseRepository.createExercise(userId, description, duration, date, (err, exercise) => {
       if (err) {
-        res.json({ error: `Error creating user ${username}: ${err}`});
+        res.json({ error: `Error creating exercise "${description}": ${err}`});
         return;
       }
-  
-      res.json(user);
+
+      res.json({
+        _id: user._id,
+        username: user.username,
+        description: exercise.description,
+        duration: exercise.duration,
+        date: exercise.date.toDateString()
+      })
     });
   });
-})
+});
+
+const getRequiredBodyProperty = (req, propertyName) => {
+  const prop = req.body[propertyName];
+
+  if (!prop) {
+    res.error({ error: `"${propertyName}" in body request is required` });
+    return;
+  }
+
+  return prop;
+}
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
